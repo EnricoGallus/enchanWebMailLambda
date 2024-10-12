cff1a33fa166e1519b592f4f5fa5e92e37142a32
enrico <enrico.gallus@gmail.com>
2024-10-12 13:21:39 +0900

    fix status code response add verification of captcha token

--- index.mjs
+++ index.mjs
@@ -1,14 +1,47 @@
 import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
+import axios from 'axios';
 const sesClient = new SESClient({ region: 'us-east-1' });
+
 const FROM_EMAIL = process.env.FROM_EMAIL;
 const TO_EMAIL = process.env.TO_EMAIL;
+const reCapUrl = 'https://www.google.com/recaptcha/api/siteverify';
+const reCaptchaSecret = process.env.RECAPTCHA_SECRET_KEY;
+const accessControlAllowOrigin = process.env.ACCESS_CONTROL_ALLOW_ORIGIN;
+const reCaptchaThreshold = 0.7;
 
-export const handler = async (inquireData) => {
-    if (!inquireData.name || !inquireData.email || !inquireData.message) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ message: 'Missing required fields' }),
-        };
+const response = (statusCode, message) => {
+    return {
+        statusCode: statusCode,
+        headers: {
+            'Access-Control-Allow-Origin': accessControlAllowOrigin,
+        },
+        body: JSON.stringify({ message: message }),
+    }
+}
+
+export const handler = async (event) => {
+    let inquireData;
+    try {
+        inquireData = JSON.parse(event.body);
+    } catch (error) {
+        return response(400, 'Invalid request body');
+    }
+
+    if (!inquireData.name || !inquireData.email || !inquireData.message || !inquireData.token) {
+        return response(400, 'Missing required fields');
+    }
+
+    let verifyResult = await axios({
+        method: 'post',
+        url: reCapUrl,
+        params: {
+            secret: reCaptchaSecret,
+            response: inquireData.token
+        }
+    })
+
+    if (!verifyResult.data.success || verifyResult.data.score < reCaptchaThreshold) {
+        return response(400, 'Invalid CAPTCHA');
     }
 
     const params = {
@@ -34,14 +67,8 @@ export const handler = async (inquireData) => {
 
     try {
         await sesClient.send(sendEmailCommand);
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: 'Email sent successfully' }),
-        };
+        return response(200, 'Email sent successfully');
     } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: 'Error sending email' }),
-        };
+        return response(500, 'Error sending email');
     }
 };
